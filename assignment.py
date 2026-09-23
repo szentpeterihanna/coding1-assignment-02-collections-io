020ce093a0c3aeda5fd08d634b7241e63af264cc
Hanna Szentpeteri <szentpeterihanna@gmail.com>
2026-09-23 17:26:00 +0200

    Update comments

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea" ## What is stored in the variable?
-sales = {"tea": 3, "coffee": 5} #what data structure is created and what does it contain?
-sold = sales[drink] ## How is the dictionary accessed here?
-sold = sold + 1 ## How does the stored value change?
-print(drink) # What is this output doing?
-print(sold)
+drink = "tea"  # Store the string "tea" 
+sales = {"tea": 3, "coffee": 5} #
+sold = sales[drink] # Look up the value
+sold = sold + 1 #
+print(drink) # print the selected drink
+print(sold) # print the number sold
